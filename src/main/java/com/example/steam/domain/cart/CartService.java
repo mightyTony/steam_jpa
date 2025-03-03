f9393de1e2d99bc92a67eafa966be37d1f1d6a0f
Sungmin Ha <mightytonykr@gmail.com>
2025-03-03 16:40:37 +0900

    cart curd

--- src/main/java/com/example/steam/domain/cart/CartService.java
+++ src/main/java/com/example/steam/domain/cart/CartService.java
@@ -1,40 +1,20 @@
 package com.example.steam.domain.cart;
 
-import com.example.steam.domain.cart.query.CartRepository;
-import com.example.steam.domain.game.Game;
-import com.example.steam.domain.game.query.GameRepository;
+import com.example.steam.domain.cart.dto.CartViewResponse;
 import com.example.steam.domain.user.User;
-import com.example.steam.exception.ErrorCode;
-import com.example.steam.exception.SteamException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class CartService {
+import java.util.List;
 
-    private final CartRepository cartRepository;
-    private final GameRepository gameRepository;
-    @Transactional
-    public void addToCart(Long gameId, User user) {
-        // 게임 조회
-        Game game = gameRepository.findById(gameId)
-                .orElseThrow(() -> new SteamException(ErrorCode.NOT_FOUND_GAME));
+public interface CartService {
 
-        // 카트에 이미 있는지 중복 검사
-        if(cartRepository.existsByUserAndGame(user, game)) {
-            throw new SteamException(ErrorCode.NOT_FOUND_GAME_IN_CART);
-        }
+    /* 장바구니에 게임 추가
+        @param : 게임 아이디, 유저 객체
+     */
+    void addToCart(Long gameId, User user);
 
-        // 장바구니에 추가
-        Cart cart = Cart.builder()
-                .user(user)
-                .game(game)
-                .build();
+    // 장바구니 목록 조회
+    List<CartViewResponse> getCartItems(User user);
 
-        cartRepository.save(cart);
-    }
+    // 장바구니에 해당 게임 삭제
+    void deleteItemFromCart(User user, Long gameId);
 }
---
+++ src/main/java/com/example/steam/domain/cart/CartServiceImpl.java
@@ -0,0 +1,67 @@
+package com.example.steam.domain.cart;
+
+import com.example.steam.domain.cart.dto.CartViewResponse;
+import com.example.steam.domain.cart.query.CartRepository;
+import com.example.steam.domain.game.Game;
+import com.example.steam.domain.game.query.GameRepository;
+import com.example.steam.domain.user.User;
+import com.example.steam.domain.user.UserRepository;
+import com.example.steam.exception.ErrorCode;
+import com.example.steam.exception.SteamException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CartServiceImpl implements CartService {
+
+    private final CartRepository cartRepository;
+    private final GameRepository gameRepository;
+    private final UserRepository userRepository;
+    @Transactional
+    public void addToCart(Long gameId, User user) {
+        // 게임 조회
+        Game game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new SteamException(ErrorCode.NOT_FOUND_GAME));
+
+        // 카트에 이미 있는지 중복 검사
+        if(cartRepository.existsByUserAndGame(user, game)) {
+            throw new SteamException(ErrorCode.ALREADY_IN_CART);
+        }
+
+        // 장바구니에 추가
+        Cart cart = Cart.builder()
+                .user(user)
+                .game(game)
+                .build();
+
+        cartRepository.save(cart);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<CartViewResponse> getCartItems(User user) {
+        List<Cart> Items = cartRepository.findByUserId(user.getId());
+
+        return Items.stream()
+                .map(CartViewResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void deleteItemFromCart(User user, Long gameId) {
+        Cart cartItem = cartRepository.findByUserAndGameId(user, gameId)
+                .orElseThrow(() -> new SteamException(ErrorCode.NOT_FOUND_GAME_IN_CART));
+
+        cartRepository.delete(cartItem);
+    }
+
+}
