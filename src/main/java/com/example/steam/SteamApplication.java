206444abc5aab3c8344f831eb5f43f39d249c812
Sungmin Ha <mightytonykr@gmail.com>
2025-03-31 13:30:53 +0900

    redis 설정 추가

--- src/main/java/com/example/steam/SteamApplication.java
+++ src/main/java/com/example/steam/SteamApplication.java
@@ -2,6 +2,7 @@ package com.example.steam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -10,6 +11,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableJpaAuditing
+@EnableCaching
 public class SteamApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/steam/config/RedisCacheConfig.java
@@ -0,0 +1,37 @@
+package com.example.steam.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    // 여기서 생성한 redisCacheManager(rcm) 메서드를 앞으로 사용 할 레디스 어노테이션에 명시 해 주어야 한다.
+    @Bean
+    public CacheManager rcm(RedisConnectionFactory redisConnectionFactory) {
+        // 레디스 캐시 기본 설정
+        // 기본 설정 key : string, value : object , ttl 10 분
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(10));
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
+}
