c1287799c49bf426333d14ca129fe73c017ea1df
Sungmin Ha <mightytonykr@gmail.com>
2025-04-10 18:52:43 +0900

    swaggerConfig fix 2

--- src/main/java/com/example/steam/config/SwaggerConfig.java
+++ src/main/java/com/example/steam/config/SwaggerConfig.java
@@ -92,7 +92,8 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(info)
                 .addSecurityItem(securityRequirement)
-                .addServersItem(new Server().url("https://spring.tonyworld.kr"))
+//                .addServersItem(new Server().url("https://spring.tonyworld.kr"))
+                .addServersItem(new Server().url("https://tonyworld.kr"))
                 .components(components);
     }
 }
